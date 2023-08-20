a1397bae7b9ec082092d6d425252bf334f0da76e
User-PC <arnela.ombasa@stu.ibu.edu.ba>
2023-08-20 11:38:54 +0200

    login & JWT

--- controler/auth.controler.js
+++ controler/auth.controler.js
@@ -1,7 +1,8 @@
 import fs from 'fs';
 import bcrypt from 'bcrypt';
-import { SALT_ROUNDS } from '../constants.js';
+import { SALT_ROUNDS, SECRET } from '../constants.js';
 import { v4 as uuidv4 } from 'uuid';
+import jwt from 'jsonwebtoken';
 
 export const registerUser = async(req, res) => {
     const {password,...user}=req.body;
@@ -18,7 +19,7 @@ export const registerUser = async(req, res) => {
             };
         parsedDb.users.push(userToSave);
        
-        fs.writeFileSync('./db.json', JSON.stringify(parsedDb));
+        fs.writeFileSync('./db.json', JSON.stringify(parsedDb,null,'\t'));
         
         res.status(201).send({...user, id: userToSave.id});
     }
@@ -27,3 +28,43 @@ export const registerUser = async(req, res) => {
         res.status(500).send('something went wrong');
     }
 };
+
+export const loginUser = async(req, res) => {
+    const{email,password}=req.body;
+   
+    let db = fs.readFileSync('./db.json', 'utf-8');
+    const parsedDb=JSON.parse(db);
+    const user=parsedDb.users.find((user)=>user.email===email);
+    const match=await bcrypt.compare(password,user.password);
+    if (match) {
+        const token = jwt.sign(
+            {
+                data: { id: user.id }
+            },
+            SECRET,
+            { expiresIn: 60 * 60 }
+        );
+        console.log(token); // Just for debugging purposes
+        res.status(200).json({ token: token, message: 'Logged in' });
+    } else {
+        res.status(401).send('Login failed');
+    }
+    
+}
+
+export const validate = (req, res) => {
+    const {token}= req.body;
+    try{
+        const decoded=jwt.verify(token,SECRET);
+       if(decoded){
+           res.status(200).send('Valid token');
+       } else{
+              res.status(401).send('Something went wrong');
+       }
+    }
+    catch(e){
+        res.status(401).send('Invalid token');
+    }
+
+
+}
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,8 @@
 import express from 'express';
-import { registerUser } from '../controler/auth.controler.js';
+import { registerUser,loginUser,validate } from '../controler/auth.controler.js';
 const router = express.Router();
 
 router.post('/register',registerUser);
-
+router.post('/login',loginUser);
+router.post('/validate',validate);
 export default router;
